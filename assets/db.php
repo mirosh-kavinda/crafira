3b80d896b9518a6a05d75ccec64b3f36783fdfb4
mirosh_kavinda <kavindahgm@gmail.com>
2022-11-06 21:20:52 +0530

    add database to website using heroku

--- assets/db.php
+++ assets/db.php
@@ -1,6 +1,6 @@
 <!-- our database load with this code -->
 <?php
-$con = new mysqli("localhost", "root", "", "crafira");
+$con = new mysqli("us-cdbr-east-06.cleardb.net", "b802d73e127878", "b128cee8", "heroku_7141704bd3c0dd2");
 
 if ($con->connect_error) {
   die("Failed to connect  :" . $con->connect_error);
